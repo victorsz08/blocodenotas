06bf284a76731e71c4ebdaa695a64626e95b4051
victorsz08 <joaovictorsal777@gmail.com>
2024-06-13 13:07:47 -0300

    Ajustes no visual e validações na busca por cep, adição de calculadora na page e links para sites externos

--- src/components/Note/index.tsx
+++ src/components/Note/index.tsx
@@ -10,6 +10,6 @@ export default function Note() {
 
     return (
     <textarea value={value} onChange={e => setValue(e.target.value)} className='background-accent' 
-    style={{ height: '560px', width: '580px', resize: 'none', 
-        fontWeight: '700', border: 'none', padding:'1.2rem', fontSize: '.9rem', borderRadius: '1.2rem'}}/>
+    style={{ height: '100vh', width: '580px', resize: 'none', 
+        fontWeight: '700', border: 'none',boxShadow: '0 0 15px #d6d6d6' , padding:'1.2rem', fontSize: '.9rem', borderRadius: '1.2rem'}}/>
 )}
--- src/components/SearchCep/index.tsx
+++ src/components/SearchCep/index.tsx
@@ -1,42 +1,7 @@
-import styled from "styled-components"
 import { FaHouse } from "react-icons/fa6";
 import { useState } from "react";
+import style from "./search.module.css";
 
-const StyleSearch = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    padding: 2rem;
-    height: 100%;
-    border-radius: 1.2rem;
-
-    .response {
-        width: 45rem;
-        font-size: .8rem;
-    }
-
-    .title {
-        margin-bottom: 2rem;
-        display: flex;
-        justify-content: flex-start;
-        gap: 15px;
-        align-items: center;
-    }
-
-    .title svg {
-        color: #ff6700;
-        width: 1.5rem;
-        height: 1.5rem;
-    }
-
-    .response table {
-        margin: 2rem 0;
-    }
-
-    .response table td {
-        padding: .6rem;
-    }
-`
 
 type IData = {
     localidade: string;
@@ -46,54 +11,59 @@ type IData = {
     uf: string;
 }
 
-
 export default function SearchCep() {
-    const [data, setData] = useState('')
+    const [data, setData] = useState('');
     const [response, setResponse] = useState<IData | any>('');
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
 
-        if(!data) {
-            return
-        };
+        if (!data) {
+            return;
+        }
 
-        const res = await fetch(`https://opencep.com/v1/${data}`).then(res => res.json());
+        let cep = data.replace("-", "");
+
+        if (cep.length === 7) {
+            cep = '0' + cep;
+        }
+
+        const res = await fetch(`https://opencep.com/v1/${cep}`).then(res => res.json());
 
         setResponse(res);
     };
 
     return (
-        <StyleSearch className="box-shadow background-accent">
-            <div className="title">
-                <FaHouse/>
+        <div className={style.search_cep_container}>
+            <div className={style.title}>
+                <FaHouse />
                 <h1>Buscar Logradouros</h1>
             </div>
-            {!response ? 
-                <form className="response" onSubmit={handleSubmit}>
-                    <input className="nt-input" value={data} onChange={e => setData(e.target.value)} placeholder="Digite o cep..."/>
-                    <button className="nt-button">Buscar</button>
-                </form>    :
-                <div className="response">
+            {!response ?
+                <form className={style.response_container} onSubmit={handleSubmit}>
+                    <input value={data} onChange={e => setData(e.target.value)} placeholder="Digite o cep..." />
+                    <button >Buscar</button>
+                </form> :
+                <div className={style.data_container}>
                     <table>
                         <tr className="table-reference">
-                            <td>Logradouro</td>
-                            <td>Bairro</td>
-                            <td>Cidade</td>
-                            <td>Estado</td>
-                            <td>CEP</td>
-                        </tr>
-                        <tr className="table">
-                            <td>{response.logradouro}</td>
-                            <td>{response.bairro}</td>
-                            <td>{response.localidade}</td>
-                            <td>{response.uf}</td>
-                            <td>{response.cep}</td>
-                        </tr>
+                                <td>Logradouro</td>
+                                <td>Bairro</td>
+                                <td>Cidade</td>
+                                <td>Estado</td>
+                                <td>CEP</td>
+                            </tr>
+                            <tr className="table">
+                                <td>{response.logradouro}</td>
+                                <td>{response.bairro}</td>
+                                <td>{response.localidade}</td>
+                                <td>{response.uf}</td>
+                                <td>{response.cep}</td>
+                            </tr>
                     </table>
-                    <button className="nt-button" onClick={() => { setData(''); setResponse('')}}>Nova Busca</button>
+                    <button onClick={() => { setData(''); setResponse('') }}>Nova Busca</button>
                 </div>
-        }
-        </StyleSearch>
-    )
+            }
+        </div>
+    );
 }
