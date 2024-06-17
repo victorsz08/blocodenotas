489ef40b9540b6086b79c1727428bedcc70e5327
Victor Siqueira <131918226+victorsz08@users.noreply.github.com>
2024-06-17 16:46:15 -0300

    Update index.tsx

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -3,6 +3,7 @@ import style from "./todo.module.css";
 import { TiDelete } from "react-icons/ti";
 import { format } from "date-fns";
 
+
 const states = [
   "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MS", "MT", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"
 ];
